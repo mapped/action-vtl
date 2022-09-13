91b7f390a34c90c4bec762623449d41b429e6e7a
Ivan Sabelnikov <ivan@mapped.com>
2022-09-13 19:11:01 +0300

    1) Turn on prettier 2) Fix linter issues 3) Fix hanging promises

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ import {GetOCI} from './oci';
 import {SemVer} from './version';
 import fs from 'fs';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function isObject(obj: any): boolean {
+function isObject<T>(obj: T): boolean {
   return obj === Object(obj);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function logAndOutputObject(key: string, value: any): void {
+function logAndOutputObject<T>(key: string, value: T): void {
   if (value == null) {
     return;
   }
@@ -35,87 +33,86 @@ function logAndOutputObject(key: string, value: any): void {
   }
 }
 
-export async function run(): Promise<void> {
-  try {
-    // Log the full context
-    // NOTE: Debug output can be enabled by setting the secret ACTIONS_STEP_DEBUG=true
-    core.debug(JSON.stringify(github.context));
+async function run(): Promise<void> {
+  // Log the full context
+  // NOTE: Debug output can be enabled by setting the secret ACTIONS_STEP_DEBUG=true
+  core.debug(JSON.stringify(github.context));
 
-    // Get the base version
-    const baseVer = core.getInput('baseVersion', {required: true});
+  // Get the base version
+  const baseVer = core.getInput('baseVersion', {required: true});
 
-    // Get the branch mappings
-    const branchMappings = new Map<string, string>();
-    const mappingsLines = core.getInput('branchMappings').split('\n');
-    for (const mapping of mappingsLines) {
-      const mappingParts = mapping.trim().split(':');
-      branchMappings.set(mappingParts[0].toLowerCase(), mappingParts[1]);
-    }
-
-    // Get the pre-release prefix
-    const preReleasePrefix = core.getInput('prereleasePrefix') ?? '';
-
-    // Get the docker image name prefix
-    const dockerImage = core.getInput('dockerImage') ?? '';
-
-    // Get the github token
-    const gitHubToken = core.getInput('gitHubToken') ?? '';
-
-    // Get releases branch
-    const releasesBranch = core.getInput('releasesBranch')?.trim() ?? '';
-
-    // Get a value indicating whether to increment patch if there is no changes detected since previous release
-    const forcePatchIncrementIfNoChanges =
-      core.getInput('forcePatchIncrementIfNoChanges')?.trim()?.toLowerCase() === 'true';
-
-    // Create a release tag
-    const createReleaseTagRes = await CreateReleaseTag(
-      github.context,
-      gitHubToken,
-      releasesBranch,
-      baseVer,
-      forcePatchIncrementIfNoChanges,
-    );
-
-    // Process the input
-    const verInfo = await SemVer(
-      createReleaseTagRes.getBaseVersionOverride(),
-      createReleaseTagRes.isPrerelease(),
-      branchMappings,
-      preReleasePrefix,
-      github.context,
-    );
-
-    const ociInfo = await GetOCI(verInfo, github.context);
-
-    // Log and push the values back to the workflow runner
-    logAndOutputObject('release_tag', createReleaseTagRes.createdReleaseTag?.toString());
-    logAndOutputObject('release_previousTag', createReleaseTagRes.previousReleaseTag.toString());
-    logAndOutputObject('ver', verInfo);
-    logAndOutputObject('oci', ociInfo);
-
-    // Add docker tags
-    if (dockerImage != null && dockerImage.length > 0) {
-      const dockerInfo = await GetDockerInfo(dockerImage, verInfo, github.context, gitHubToken);
-      logAndOutputObject('docker', dockerInfo);
-    }
-
-    // Write out the version file
-    const verFile = core.getInput('versionFile');
-    fs.writeFile(verFile, verInfo.semVer, {encoding: 'utf8'}, function (err) {
-      if (err) {
-        throw err;
-      }
-
-      core.info(`Wrote semver to ${verFile}`);
-    });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      core.setFailed(error.message);
-    } else {
-      core.setFailed('unexpected error');
-    }
+  // Get the branch mappings
+  const branchMappings = new Map<string, string>();
+  const mappingsLines = core.getInput('branchMappings').split('\n');
+  for (const mapping of mappingsLines) {
+    const mappingParts = mapping.trim().split(':');
+    branchMappings.set(mappingParts[0].toLowerCase(), mappingParts[1]);
   }
+
+  // Get the pre-release prefix
+  const preReleasePrefix = core.getInput('prereleasePrefix') ?? '';
+
+  // Get the docker image name prefix
+  const dockerImage = core.getInput('dockerImage') ?? '';
+
+  // Get the github token
+  const gitHubToken = core.getInput('gitHubToken') ?? '';
+
+  // Get releases branch
+  const releasesBranch = core.getInput('releasesBranch')?.trim() ?? '';
+
+  // Get a value indicating whether to increment patch if there is no changes detected since previous release
+  const forcePatchIncrementIfNoChanges =
+    core.getInput('forcePatchIncrementIfNoChanges')?.trim()?.toLowerCase() === 'true';
+
+  // Create a release tag
+  const createReleaseTagRes = await CreateReleaseTag(
+    github.context,
+    gitHubToken,
+    releasesBranch,
+    baseVer,
+    forcePatchIncrementIfNoChanges,
+  );
+
+  // Process the input
+  const verInfo = await SemVer(
+    createReleaseTagRes.getBaseVersionOverride(),
+    createReleaseTagRes.isPrerelease(),
+    branchMappings,
+    preReleasePrefix,
+    github.context,
+  );
+
+  const ociInfo = await GetOCI(verInfo, github.context);
+
+  // Log and push the values back to the workflow runner
+  logAndOutputObject('release_tag', createReleaseTagRes.createdReleaseTag?.toString());
+  logAndOutputObject('release_previousTag', createReleaseTagRes.previousReleaseTag.toString());
+  logAndOutputObject('ver', verInfo);
+  logAndOutputObject('oci', ociInfo);
+
+  // Add docker tags
+  if (dockerImage != null && dockerImage.length > 0) {
+    const dockerInfo = await GetDockerInfo(dockerImage, verInfo, github.context, gitHubToken);
+    logAndOutputObject('docker', dockerInfo);
+  }
+
+  // Write out the version file
+  const verFile = core.getInput('versionFile');
+  fs.writeFile(verFile, verInfo.semVer, {encoding: 'utf8'}, function (err) {
+    if (err) {
+      throw err;
+    }
+
+    core.info(`Wrote semver to ${verFile}`);
+  });
 }
 
-run();
+// eslint-disable-next-line github/no-then
+run().catch((error: unknown) => {
+  if (error instanceof Error) {
+    core.setFailed(error.message);
+  } else {
+    core.setFailed('unexpected error');
+  }
+});
