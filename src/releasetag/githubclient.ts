b05d7dc7c6c703e7f3b2653f7558c20dde8498b0
Ivan Sabelnikov <ivan@mapped.com>
2022-09-13 17:20:24 +0300

    Throw error on not ok status

--- src/releasetag/githubclient.ts
+++ src/releasetag/githubclient.ts
@@ -1,5 +1,6 @@
 import * as github from '@actions/github';
 import {Octokit} from '@octokit/core';
+import {OctokitResponse} from '@octokit/types';
 
 export class GitHubClient {
   private octokit: Octokit;
@@ -15,6 +16,8 @@ export class GitHubClient {
       per_page: 100, // There might be some custom tags. Take the maximum amount of items to avoid searching for the valid latest release through several pages
     });
 
+    this.throwIfNotOk(res);
+
     return res.data;
   }
 
@@ -26,25 +29,41 @@ export class GitHubClient {
       per_page: 100, // Do not search for the latest release commit forever
     });
 
+    this.throwIfNotOk(res);
+
     return res.data;
   }
 
   async createTag(tagName: string, comments: string, commitSha: string): Promise<void> {
-    await this.octokit.request('POST /repos/{owner}/{repo}/git/tags', {
-      owner: this.owner,
-      repo: this.repo,
-      tag: tagName,
-      message: comments,
-      object: commitSha,
-      type: 'commit',
-    });
+    this.throwIfNotOk(
+      await this.octokit.request('POST /repos/{owner}/{repo}/git/tags', {
+        owner: this.owner,
+        repo: this.repo,
+        tag: tagName,
+        message: comments,
+        object: commitSha,
+        type: 'commit',
+      }),
+    );
 
-    await this.octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-      owner: this.owner,
-      repo: this.repo,
-      ref: `refs/tags/${tagName}`,
-      sha: commitSha,
-    });
+    this.throwIfNotOk(
+      await this.octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+        owner: this.owner,
+        repo: this.repo,
+        ref: `refs/tags/${tagName}`,
+        sha: commitSha,
+      }),
+    );
+  }
+
+  throwIfNotOk(response: OctokitResponse<any, number>): void {
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(
+        `Unexpected status code while calling GitHub API: ${
+          response.status
+        }. Data: ${JSON.stringify(response.data)}`,
+      );
+    }
   }
 }
 
