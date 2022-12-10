8ab8f165940757ecfcb1b5fe2b2d90b1f936e14d
shaunco <365611+shaunco@users.noreply.github.com>
2022-12-10 01:24:27 +0000

    Update latest to output generated at 44f0fe0

--- dist/index.js
+++ dist/index.js
@@ -66,19 +66,27 @@ function GetDockerInfo(dockerImage, version, dockerPlatformSuffix, context, toke
         if (dockerPlatformSuffix.length > 0) {
             dockerPlatformSuffix = `-${dockerPlatformSuffix}`;
         }
+        // Get a deterministic tag
+        let dtag = '';
         // Add the version tag if it is not latest
         if (version.tag !== 'latest') {
             // Without any 'v' prefix
+            let tag = '';
             if (version_1.SEMVER_REGEX.test(version.tag) && version.tag.substring(0, 1).toLowerCase() === 'v') {
-                tags.push(`${dockerImage}:${version.tag.substring(1)}${dockerPlatformSuffix}`);
+                tag = `${dockerImage}:${version.tag.substring(1)}${dockerPlatformSuffix}`;
             }
             else {
-                tags.push(`${dockerImage}:${version.tag}${dockerPlatformSuffix}`);
+                tag = `${dockerImage}:${version.tag}${dockerPlatformSuffix}`;
             }
+            tags.push(tag);
+            dtag = tag;
         }
         // For any push we add a sha tag
         if (context.eventName === 'push') {
-            tags.push(`${dockerImage}:sha-${context.sha.substring(0, 8)}${dockerPlatformSuffix}`);
+            const tag = `${dockerImage}:sha-${context.sha.substring(0, 8)}${dockerPlatformSuffix}`;
+            tags.push(tag);
+            // SHA based is always the most deterministic, so overwrite the semver dtag from above
+            dtag = tag;
         }
         // If the version's tag is a SEMVER, we need semver stable tags
         const semVerParts = version.tag.match(version_1.SEMVER_REGEX);
@@ -86,13 +94,23 @@ function GetDockerInfo(dockerImage, version, dockerPlatformSuffix, context, toke
             // Is this a pre-release?
             if (version.preRelease && version.preRelease.length > 0) {
                 // Pre-release, only the full semver
-                tags.push(`${dockerImage}:${version.semVerNoMeta}${dockerPlatformSuffix}`);
+                const tag = `${dockerImage}:${version.semVerNoMeta}${dockerPlatformSuffix}`;
+                tags.push(tag);
+                // If we don't already have a dtag, use this
+                if (!dtag) {
+                    dtag = tag;
+                }
             }
             else {
                 // Not a pre-release, get all stable tags
                 tags.push(`${dockerImage}:${version.major}${dockerPlatformSuffix}`);
                 tags.push(`${dockerImage}:${version.major}.${version.minor}${dockerPlatformSuffix}`);
-                tags.push(`${dockerImage}:${version.major}.${version.minor}.${version.patch}${dockerPlatformSuffix}`);
+                const fulltag = `${dockerImage}:${version.major}.${version.minor}.${version.patch}${dockerPlatformSuffix}`;
+                tags.push(fulltag);
+                // If we don't already have a dtag, use this
+                if (!dtag) {
+                    dtag = fulltag;
+                }
                 // Tagged build gets the 'latest' tag if it is the highest semver tag created
                 if (releases) {
                     // Look through all the releases for a newer tag
@@ -123,6 +141,7 @@ function GetDockerInfo(dockerImage, version, dockerPlatformSuffix, context, toke
         const uniqueTags = [...new Set(tags)];
         // Put together the output
         const dockerInfo = {
+            dtag,
             tags: uniqueTags.join(','),
             push: shouldPush.toString(),
         };
